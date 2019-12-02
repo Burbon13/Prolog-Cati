066d4e5fdb955e9897fc73c303da1f9f25233a8c
Razvan <razvan_roatis@yahoo.com>
2019-12-02 22:57:23 +0200

    Implemented left neighbour verification

--- src/ex2.pl
+++ src/ex2.pl
@@ -1,5 +1,50 @@
 /* Exercise 2 - 15-puzzle */
 
+/* neighbours(S0, LS1) is true if LS1 is a list of upcoming states from
+ * the state S0 */
+ neighbours(S0, [NextLeft, NextTop, NextRight, NextBottom]):-
+    left_neighbour(S0, NextLeft),
+    top_neighbour(S0, NextTop),
+    right_neighbour(S0, NextRight),
+    bottom_neighbour(S0, NextBottom).
+
+
+/* left_neighbour(S0, S1) verifies that S1 is an upcoming state of S0 by one
+ * move to left; S0 and S1 should be valid states */
+left_neighbour([List, Position], [ListNeighbour, PositionNeighbour]):-
+    NextPosition is Position - 1,
+    NextPosition =:= PositionNeighbour,
+    swap_elems(List, Position, NextPosition, ListNeighbour),
+    !.
+left_neighbour([_, 1], -1).
+
+
+/* swap_elems(List1, Pos1, Pos2, List2) swapps elements on positions Pos1 and Pos2
+ * Also checks if List2 is equal to List1 after the swap between Pos1 and Pos2*/
+swap_elems(List, Pos1, Pos2, Result):-
+    get_elem_at(List, Pos1, Elem1),
+    get_elem_at(List, Pos2, Elem2),
+    set_elem_at(List, Pos1, Elem2, AuxResult),
+    set_elem_at(AuxResult, Pos2, Elem1, Result).
+
+
+/* get_elem_at(List, Pos, Elem) gets the element at position Pos from List */
+get_elem_at([H|_], 1, H):-!.
+get_elem_at([_|T], Pos, Elem):-
+    Pos > 1,
+    NextPos is Pos - 1,
+    get_elem_at(T, NextPos, Elem).
+
+
+/* set_elem_at(List, Pos, Elem, Result) sets the element at position Pos to
+ * value Elem */
+set_elem_at([_|T], 1, Elem, [Elem|T]):-!.
+set_elem_at([H|T], Pos, Elem, Result):-
+    Pos > 1,
+    NextPos is Pos - 1,
+    set_elem_at(T, NextPos, Elem, ResultAux),
+    Result = [H|ResultAux].
+
 
 /* state(X) is True if X is a term that represents a state of the problem
  * 15-puzzle */
