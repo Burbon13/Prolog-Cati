09b72333f740413b97b0fa2f5b0cc41c646d7d64
Razvan <razvan_roatis@yahoo.com>
2019-12-08 22:07:02 +0200

    Implemented push box (without restrictions)

--- src/ex3.pl
+++ src/ex3.pl
@@ -23,10 +23,10 @@ move(
         posM(4, middle),
         onbox,
         boxes(
-            posB(4, middle, 1),
-            posB(4, middle, 2),
-            posB(4, middle, 3),
-            posB(4, middle, 4)
+            posM(4, middle),
+            posM(4, middle),
+            posM(4, middle),
+            posM(4, middle)
         ),
         hasnot
         ),
@@ -35,58 +35,182 @@ move(
         posM(4, middle),
         onbox,
         boxes(
-            posB(4, middle, 1),
-            posB(4, middle, 2),
-            posB(4, middle, 3),
-            posB(4, middle, 4)
+            posM(4, middle),
+            posM(4, middle),
+            posM(4, middle),
+            posM(4, middle)
         ),
         has
     )
 ).
+
 /* === Climb box === */
 move(
     state(
         posM(4, middle),
         onfloor,
         boxes(
-            posB(4, middle, 1),
-            posB(4, middle, 2),
-            posB(4, middle, 3),
-            posB(4, middle, 4)
+            posM(4, middle),
+            posM(4, middle),
+            posM(4, middle),
+            posM(4, middle)
         ),
         hasnot
         ),
-    grasp,
+    climb,
     state(
         posM(4, middle),
         onbox,
         boxes(
-            posB(4, middle, 1),
-            posB(4, middle, 2),
-            posB(4, middle, 3),
-            posB(4, middle, 4)
+            posM(4, middle),
+            posM(4, middle),
+            posM(4, middle),
+            posM(4, middle)
         ),
         hasnot
     )
 ).
+
+/* === Push box === */
+/* Box nr. 1 */
+move(
+    state(
+        MonkeyPos1,
+        onfloor,
+        boxes(
+            MonkeyPos1,
+            -1,
+            -1,
+            -1
+        ),
+        hasnot
+    ),
+    pushBox(1, MonkeyPos1, MonkeyPos2),
+    state(
+        MonkeyPos2,
+        onfloor,
+        boxes(
+            MonkeyPos2,
+            -1,
+            -1,
+            -1
+        ),
+        hasnot
+    )
+):-
+    nex_pos(MonkeyPos1, MonkeyPos2).
+/* Box nr. 2 */
+move(
+    state(
+        MonkeyPos1,
+        onfloor,
+        boxes(
+            posM(4, middle),
+            MonkeyPos1,
+            -1,
+            -1
+        ),
+        hasnot
+    ),
+    pushBox(2, MonkeyPos1, MonkeyPos2),
+    state(
+        MonkeyPos2,
+        onfloor,
+        boxes(
+            posM(4, middle),
+            MonkeyPos2,
+            -1,
+            -1
+        ),
+        hasnot
+    )
+):-
+    nex_pos(MonkeyPos1, MonkeyPos2).
+
+/* Box nr. 3 */
+move(
+    state(
+        MonkeyPos1,
+        onfloor,
+        boxes(
+            posM(4, middle),
+            posM(4, middle),
+            MonkeyPos1,
+            -1
+        ),
+        hasnot
+    ),
+    pushBox(3, MonkeyPos1, MonkeyPos2),
+    state(
+        MonkeyPos2,
+        onfloor,
+        boxes(
+            posM(4, middle),
+            posM(4, middle),
+            MonkeyPos2,
+            -1
+        ),
+        hasnot
+    )
+):-
+    nex_pos(MonkeyPos1, MonkeyPos2).
+
+/* Box nr. 4 */
+move(
+    state(
+        MonkeyPos1,
+        onfloor,
+        boxes(
+            posM(4, middle),
+            posM(4, middle),
+            posM(4, middle),
+            MonkeyPos1,
+        ),
+        hasnot
+    ),
+    pushBox(4, MonkeyPos1, MonkeyPos2),
+    state(
+        MonkeyPos2,
+        onfloor,
+        boxes(
+            posM(4, middle),
+            posM(4, middle),
+            posM(4, middle),
+            MonkeyPos2,
+        ),
+        hasnot
+    )
+):-
+    nex_pos(MonkeyPos1, MonkeyPos2).
+
 /* === Monkey walk === */
 move(
-    state(MonkeyPos1, onfloor, BoxPos, hasnot),
+    state(
+        MonkeyPos1,
+        onfloor,
+        BoxPos,
+        hasnot
+    ),
     walk(MonkeyPos1, MonkeyPos2),
-    state(MonkeyPos2, onfloor, BoxPos, hasnot)
+    state(
+        MonkeyPos2,
+        onfloor,
+        BoxPos,
+        hasnot
+    )
 ):-
-    walker(MonkeyPos1, MonkeyPos2).
+    nex_pos(MonkeyPos1, MonkeyPos2).
 
 /* Next walking position generator */
-walker(posM(Room, middle), posM(Room, door)).
-walker(posM(Room, middle), posM(Room, window)).
-walker(posM(Room, door), posM(Room, window)).
-walker(posM(Room, door), posM(Room, middle)).
-walker(posM(Room, window), posM(Room, door)).
-walker(posM(Room, window), posM(Room, middle)).
-walker(posM(4, door), posM(1, door)).
-walker(posM(4, door), posM(2, door)).
-walker(posM(4, door), posM(3, door)).
-walker(posM(1, door), posM(4, door)).
-walker(posM(2, door), posM(4, door)).
-walker(posM(3, door), posM(4, door)).
+nex_pos(posM(Room, middle), posM(Room, door)).
+nex_pos(posM(Room, middle), posM(Room, window)).
+nex_pos(posM(Room, door), posM(Room, window)).
+nex_pos(posM(Room, door), posM(Room, middle)).
+nex_pos(posM(Room, window), posM(Room, door)).
+nex_pos(posM(Room, window), posM(Room, middle)).
+nex_pos(posM(4, door), posM(1, door)).
+nex_pos(posM(4, door), posM(2, door)).
+nex_pos(posM(4, door), posM(3, door)).
+nex_pos(posM(1, door), posM(4, door)).
+nex_pos(posM(2, door), posM(4, door)).
+nex_pos(posM(3, door), posM(4, door)).
